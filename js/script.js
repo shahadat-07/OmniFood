187081b7df7d519e091e81da545501c5c0d2df5e
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2022-01-21 10:24:02 +0600

    Image Optimized, meta tag and favicon added

--- js/script.js
+++ js/script.js
@@ -46,6 +46,33 @@ allLinks.forEach(function (link) {
   });
 });
 
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
